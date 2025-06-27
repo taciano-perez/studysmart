2871932fe6a8a938c872abb95fe84f0508aa887f
Taciano Dreckmann Perez <tdperez@hotmail.com>
2025-06-27 18:04:53 +0200

    Add study hours modal

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     now = datetime.date.today()
     cal = calendar.HTMLCalendar(calendar.MONDAY).formatmonth(now.year, now.month)
-    return render_template('index.html', calendar_html=cal)
+    return render_template('index.html', calendar_html=cal, today=now.isoformat())
 
 if __name__ == '__main__':
     app.run(debug=True)
